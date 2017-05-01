be5d8edc5e8deaf976cc61a2e87574c662358096
hiba akel <hibaakel226@gmail.com>
2017-05-01 13:09:39 +0300

    begin handlebars #45

--- src/server.js
+++ src/server.js
@@ -1,17 +1,27 @@
 const express = require('express');
 const path = require('path');
-
+const exphbs = require('express-handlebars');
 const app = express();
 
+app.engine('.hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs'
+}));
+
 app.set('port', process.env.PORT || 3000);
+app.set('view engine', '.hbs');
 
 const options = {
   dotfiles: 'ignore',
   extensions: ['htm', 'html'],
-  index: 'main.html'
+  index: false
 };
 
 app.use(express.static(path.join(__dirname, '../public'), options));
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
 
 app.listen(app.get('port'), () => {
   console.log('Express server running on port: ', app.get('port'));
---
+++ src/views/home.hbs
@@ -0,0 +1,1 @@
+<h1>hi everybody</h1>
