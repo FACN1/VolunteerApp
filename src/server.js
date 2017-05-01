d3d1986e579d63d46f99a30b2f303f98e2c4fc65
Matt Davis <matthewpeterdavis@gmail.com>
2017-05-01 12:17:08 +0300

    add nodemon and dev script

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello هبة');
+  res.send('Hello هبة, I\'m Mavis');
 });
 
 app.listen(3000, () => {
