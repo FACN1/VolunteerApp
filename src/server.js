d05a332f9ba4a05adc354b7a2239bdb4a36d8a4c
Matt Davis <matthewpeterdavis@gmail.com>
2017-05-02 15:55:29 +0300

    when directing to /list, connect to db and render list #51

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
+const mongodb = require('mongodb');
+require('env2')('./config.env');
+
 const app = express();
 
 app.engine('.hbs', exphbs({
@@ -22,8 +25,29 @@ app.use(express.static(path.join(__dirname, '../public'), options));
 app.get('/', (req, res) => {
   res.render('home');
 });
+
 app.get('/list', (req, res) => {
-  res.render('list');
+  const MongoClient = mongodb.MongoClient;
+  const url = process.env.MONGODB_URI;
+
+  MongoClient.connect(url, (err, db) => {
+    if (err) return ('err: ', err);
+    else {
+      console.log('connection made');
+      const collection = db.collection('vol_roles');
+      collection.find({}).toArray((err, result) => {
+        if (err) res.send(err);
+        else if (result.length) {
+          res.render('list', {
+            'roleList': result
+          });
+        } else {
+          res.send('No roles found');
+        }
+        db.close();
+      });
+    }
+  });
 });
 app.listen(app.get('port'), () => {
   console.log('Express server running on port: ', app.get('port'));
