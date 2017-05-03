2ee82461596c8604f956cd1ab0ec4bf25b99f639
hiba akel <hibaakel226@gmail.com>
2017-05-03 17:14:28 +0300

    make validation information for orgform #70

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const mongodb = require('mongodb');
 const ObjectId = require('mongodb').ObjectID;
 require('env2')('./config.env');
 const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
 const app = express();
 const MongoClient = mongodb.MongoClient;
 const url = process.env.MONGODB_URI;
@@ -15,6 +16,22 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+// set up expressValidator
+app.use(expressValidator({
+  errorFormatter: (param, msg, value) => {
+    let namespace = param.split('.');
+    let root = namespace.shift();
+    let formParam = root;
+    while (namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param: formParam,
+      msg: msg,
+      value: value
+    };
+  }
+}));
 app.engine('.hbs', exphbs({
   defaultLayout: 'main',
   extname: '.hbs',
@@ -89,33 +106,40 @@ app.get('/orgform', (req, res) => {
 });
 
 app.post('/addrole', (req, res) => {
-  MongoClient.connect(url, (err, db) => {
-    if (err) return ('Error connection to DB: ', err);
-    else {
-      console.log('connection made');
-      // object take the data from html page and put in this object
-      const role = {
-        'org_name': req.body.org_name,
-        'org_desc': req.body.org_desc,
-        'phone_num': req.body.user_phone,
-        'email': req.body.user_mail,
-        'role_name': req.body.role_name,
-        'role_desc': req.body.role_desc,
-        'num_vlntr_req': req.body.num_vol,
-        'start_date': req.body.start_date,
-        'end_date': req.body.end_date
-      };
-      // connect to the table called vol_roles
-      const collection = db.collection('vol_roles');
-      // insert the data in db
-      collection.insert(role, {w: 1}, (err, result) => {
-        if (err) return ('Error inserting to DB: ', err);
-        db.close();
-        // redirect the information to the list page also
-        res.redirect('/list');
-      });
-    }
-  });
+  req.checkBody('org_name', 'Organisation name required').notEmpty().isAlpha();
+  const errors = req.validationErrors();
+  if (errors) {
+    console.log(errors);
+    res.redirect('/orgform');
+  } else {
+    MongoClient.connect(url, (err, db) => {
+      if (err) return ('Error connection to DB: ', err);
+      else {
+        console.log('connection made');
+        // object take the data from html page and put in this object
+        const role = {
+          'org_name': req.body.org_name,
+          'org_desc': req.body.org_desc,
+          'phone_num': req.body.user_phone,
+          'email': req.body.user_mail,
+          'role_name': req.body.role_name,
+          'role_desc': req.body.role_desc,
+          'num_vlntr_req': req.body.num_vol,
+          'start_date': req.body.start_date,
+          'end_date': req.body.end_date
+        };
+        // connect to the table called vol_roles
+        const collection = db.collection('vol_roles');
+        // insert the data in db
+        collection.insert(role, {w: 1}, (err, result) => {
+          if (err) return ('Error inserting to DB: ', err);
+          db.close();
+          // redirect the information to the list page also
+          res.redirect('/list');
+        });
+      }
+    });
+  }
 });
 app.post('/addvolunteer', (req, res) => {
   MongoClient.connect(url, (err, db) => {
