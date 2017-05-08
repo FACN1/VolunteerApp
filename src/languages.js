011b3c553b1446a9acc6e6c8267626ff28d315ca
Matt Davis <matthewpeterdavis@gmail.com>
2017-05-08 10:40:17 +0300

    add language file with object of languages

---
+++ src/languages.js
@@ -0,0 +1,12 @@
+module.exports = {
+  arabic: {
+    title: 'شجرة الحياة',
+    description: 'هذا التطبيق عبارة عن منصة بين كل من يرغب بالتطوع ويبحث عن اطار ملائم وجمعيات باحثة عن متطوعين',
+    frontPageButton: 'متطوع'
+  },
+  english: {
+    title: 'Tree Of Life',
+    description: 'An app to help connect volunteers with organisation look for them',
+    frontPageButton: 'Volunteer!'
+  }
+};
