4267763e811a9768364eb26489cfe28d646dae32
Matt Davis <matthewpeterdavis@gmail.com>
2017-05-08 11:21:39 +0300

    add more keys to the language files

--- src/languages.js
+++ src/languages.js
@@ -1,12 +1,50 @@
 module.exports = {
   arabic: {
+    // home page
     title: 'شجرة الحياة',
     description: 'هذا التطبيق عبارة عن منصة بين كل من يرغب بالتطوع ويبحث عن اطار ملائم وجمعيات باحثة عن متطوعين',
-    frontPageButton: 'متطوع'
+    frontPageButton: 'متطوع',
+    // list page
+    listHeader: '',
+    roleDesc: '',
+    OrgDesc: '',
+    moreInfoBtn: '',
+    // volunteer form page
+    formHeader: 'Application Form',
+    formFN: 'First Name',
+    formLN: 'Last Name',
+    formAge: 'Age:',
+    formAvail: 'Please indicate what days and times you would be avaliable: ',
+    formPhone: 'Phone Number',
+    formEmail: 'Email Address:',
+    formSubmit: 'Submit Details',
+    // datasubmit page
+    submitHeader: 'العملية تمت بنجاح',
+    // org form page
+    orgFormHeader: 'Fill this With headline about Org Form'
   },
   english: {
     title: 'Tree Of Life',
     description: 'An app to help connect volunteers with organisation look for them',
-    frontPageButton: 'Volunteer!'
+    frontPageButton: 'Volunteer!',
+    // list page
+    listHeader: 'Opportunities',
+    roleDesc: 'Role Description: ',
+    OrgDesc: 'Organisation Description: ',
+    moreInfoBtn: 'More Info',
+    applyButton: 'Apply',
+    // volunteer form page
+    formHeader: 'Application Form',
+    formFN: 'First Name',
+    formLN: 'Last Name',
+    formAge: 'Age:',
+    formAvail: 'Please indicate what days and times you would be avaliable: ',
+    formPhone: 'Phone Number',
+    formEmail: 'Email Address:',
+    formSubmit: 'Submit Details',
+    // datasubmit page
+    submitHeader: '',
+    // org form page
+    orgFormHeader: 'Fill this With headline about Org Form'
   }
 };
