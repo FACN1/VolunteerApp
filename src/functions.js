1c7e9eef4f42c83a5dbbe184dfc1674f7f738ae7
Matt Davis <matthewpeterdavis@gmail.com>
2017-05-10 12:22:33 +0300

    make function to convert date

---
+++ src/functions.js
@@ -0,0 +1,8 @@
+module.exports = {
+  convertDate: (date) => {
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  }
+};
